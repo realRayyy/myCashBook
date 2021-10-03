bebd70d46968626fb37116ee89226b4a52ffb372
realRayyy <91679915+realRayyy@users.noreply.github.com>
2021-10-03 11:00:43 +0800

    Add files via upload

--- js/btn-click.js
+++ js/btn-click.js
@@ -1,13 +1,70 @@
-var btn = document.getElementById("search");
+function getTop(e){
+    var offset=e.offsetTop;
+    if(e.offsetParent != null) offset += getTop(e.offsetParent);
+    return offset;
+}
 
-function ripple(el){
-    let x = el.touches[0].clientX - this.offsetLeft;             
-    let y = el.touches[0].clientY - this.offsetTop;           
+function getLeft(e){
+    var offset=e.offsetLeft;
+    if(e.offsetParent != null) offset += getLeft(e.offsetParent);
+    return offset;
+}
+
+var btnSearch = document.getElementById("search");
+var cirSearch;
+
+function ripple(el, btn){
+    let x = el.touches[0].clientX - btn.offsetLeft;             
+    let y = el.touches[0].clientY - btn.offsetTop;
+    console.log(el.touches[0].clientX);
+    console.log(btn.offsetLeft);
+    console.log(btn.offsetParent);
+    console.log(el.touches[0].clientY);
+    console.log(btn.offsetTop); 
+    console.log(getLeft(btn));
+    console.log(getTop(btn));          
     let circle = document.createElement('span');
     circle.className = 'ripple';       
     circle.style.left = x + 'px';     
     circle.style.top = y + 'px';   
     btn.appendChild(circle);
+    return circle;
 }
 
-btn.addEventListener('touchstart', ripple);
+function removeRipple(btn, circle){
+    btn.removeChild(circle);
+}
+
+btnSearch.addEventListener('touchstart', function(el){
+    cirSearch = ripple(el, btnSearch);
+});
+
+btnSearch.addEventListener('touchend' ,function(el){
+    setTimeout("removeRipple(btnSearch, cirSearch)", 100);
+});
+
+var btnEye = document.getElementById("eye");
+var cirEye;
+
+btnEye.addEventListener('touchstart', function(el){
+    el.stopPropagation();
+    cirEye = ripple(el, btnEye);
+});
+
+btnEye.addEventListener('touchend' ,function(el){
+    el.stopPropagation();
+    setTimeout("removeRipple(btnEye, cirEye)", 100);
+});
+
+var btnContent = document.getElementById("content");
+var cirContent;
+
+btnContent.addEventListener('touchstart', function(el){
+    el.stopPropagation();
+    cirContent = ripple(el, btnContent);
+});
+
+btnContent.addEventListener('touchend' ,function(el){
+    el.stopPropagation();
+    setTimeout("removeRipple(btnContent, cirContent)", 100);
+});
